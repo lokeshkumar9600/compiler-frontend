217a4358627c8d67515d1399ca9684dde26a036f
lokeshkumar9600 <lokesh960077152@gmail.com>
2023-01-13 00:16:27 +0530

    added execution indicator

--- src/Editor.js
+++ src/Editor.js
@@ -1,15 +1,12 @@
-
 import {React, useState } from 'react';
 import axios from "axios"
 import "./App.css";
-
 const Editor = () => {
-  
     const [Code,setCode] = useState("");
     const [output,setOutput] = useState("")
     let [language,setLanguage] = useState("java")
-
-   
+    let [executing,setExecuting] = useState(false)
+    
     const LanguageObject = {
       "Java":"java",
        "C":"c",
@@ -21,35 +18,35 @@ const Editor = () => {
     }
    const  showOutput = async ()=>
    {
-    console.log(language);
-    console.log(Code);
+    setOutput("")
+    setExecuting(true);
     const result = await axios.get('https://lovely-pink-walkingstick.cyclic.app/getOutput', {
         params: {
           code: Code,
           language: language
         }
       });
-
       setOutput(result.data.data.output);
-      console.log(result.data.data.output);
-      console.log(result)
-      console.log(result.data)
-      
+      setExecuting(false)
    }
-
   return (
     <div className="main">
         <div className="container">
         <div className="main-editor">
            <textarea  className="editor-form" onChange={(e)=>setCode(e.target.value)}></textarea>
       </div>
+      <div className="editor-output">
       <div className="main-editor-output">
            <textarea  className="editor-form-output" onChange={(e)=>setCode(e.target.value)} disabled value={output}></textarea>
+      </div>
       </div>
         </div>
         <div className="editor-controls">
         <div type="button" onClick={showOutput} className="editor-execute">
         <p className="word">Execute Code</p>
+        </div>
+        <div type="button" onClick={showOutput} className="editor-execute" style={{"margin-left":"10px"}}>
+        <p className="word">Check GPT plagiarism</p>
         </div>
         <select className='editor-language-selector' onChange={(e)=>setLanguage(e.target.value)}>
       {Object.keys(LanguageObject).map((key)=>{
@@ -59,7 +56,9 @@ const Editor = () => {
       })}
         </select>
         </div>
-     
+        <div className="execution-indicator">
+        {output ? <p>Execution complete</p> : executing ? <p>Executing...</p> : <p></p>}
+        </div>
     </div>
   )
 }
